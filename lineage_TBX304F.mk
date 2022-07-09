42157a0f372f2ef4efbacd6e7d2c014133924ccf
deadman96385 <seanhoyt963@gmail.com>
2022-07-09 10:04:30 -0500

    TBX304F: Swap Screen width/height
    This makes the boot animation and default rotation landscape

--- lineage_TBX304F.mk
+++ lineage_TBX304F.mk
@@ -15,8 +15,8 @@
 #
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
 
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
